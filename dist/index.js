257aca278ed88975abe376b333208f5c5100221f
Santi Liwsakphaiboon <oofpokemon@gmail.com>
2021-02-24 17:45:41 +0700

    fix bootstrap import, update version

--- dist/index.js
+++ dist/index.js
@@ -10,8 +10,6 @@ var _App = _interopRequireDefault(require("./App"));
 
 var _reportWebVitals = _interopRequireDefault(require("./reportWebVitals"));
 
-require("bootstrap/dist/css/bootstrap.min.css");
-
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
 _reactDom["default"].render( /*#__PURE__*/_react["default"].createElement(_react["default"].StrictMode, null, /*#__PURE__*/_react["default"].createElement(_App["default"], null)), document.getElementById('root')); // If you want to start measuring performance in your app, pass a function
